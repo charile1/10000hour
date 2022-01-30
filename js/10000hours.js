4e7334c519ac5d22b3762136775d77bf2e4c8094
Kyungsuh Park <pyo05360@gmail.com>
2022-01-30 22:17:46 +0900

    마크업 완성

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,5 @@
-// js file
+const startButton = document.querySelector(".start_btn")
+const job = document.querySelector(".explain input:first-child");
+console.log(job)
+const time = document.querySelector(".explain input:last-child");
+console.log(time)
